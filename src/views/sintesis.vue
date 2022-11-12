b8bef6e3305d773e98c4d199a89eb32706ad8128
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2022-11-12 00:20:01 -0500

    Se elimina logo de la síntesis.

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,15 +3,6 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    .row.mb-5.align-items-center
-      .col-lg-10
-        .titulo-sexto.color-acento-contenido.color-adicional-1.mb-5
-          h5 Gestión comunitaria del riesgo de desastres.
-          br
-          | Síntesis: Planeación y promotoría para gestión del riesgo en comunidades.
-
-      .col-lg-2
-        img.img-a(src="@/assets/curso/temas/img_sin.png", alt="alt")
 
     p.mb-5 A continuación, se presenta un mapa conceptual en el cual se encontrarán los contenidos trabajados en el componente formativo.
 
